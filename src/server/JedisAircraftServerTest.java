087471751725571b9b712257c79efb0ce5cff21a
IamDiesel <Daniel.Kahrizi@gmx.de>
2015-05-17 22:35:40 +0200

    Test JedisAircraftServer
    Checking for publish and set mechanism

--- src/server/JedisAircraftServerTest.java
+++ src/server/JedisAircraftServerTest.java
@@ -1,7 +1,14 @@
 package server;
 
-import org.junit.*;
 import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import domain.AirborneVelocityMessage;
+import factory.AdsMessageFactory;
+import redis.clients.jedis.Jedis;
 
 /**
  * The class <code>JedisAircraftServerTest</code> contains tests for the class <code>{@link JedisAircraftServer}</code>.
@@ -20,30 +27,33 @@ public class JedisAircraftServerTest {
 	public void testJedisAircraftServer_1()
 		throws Exception {
 		JedisAircraftServer result = new JedisAircraftServer();
-		assertNotNull(result);
+		
 		// add additional test code here
 	}
-
-	/**
-	 * Run the void onMessage(String,String) method test.
-	 *
-	 * @throws Exception
-	 *
-	 * @generatedBy CodePro at 17.05.15 21:38
-	 */
+	
 	@Test
 	public void testOnMessage_1()
 		throws Exception {
-		JedisAircraftServer fixture = new JedisAircraftServer();
-		String channel = "ads.msg.identification";
-		String message = "";
-
-		fixture.onMessage(channel, message);
-
-		// add additional test code here
-		// An unexpected exception was thrown in user code while executing this test:
-		//    java.lang.ArrayIndexOutOfBoundsException: 1
-		//       at server.JedisAircraftServer.onMessage(JedisAircraftServer.java:21)
+		JedisAircraftServer fixture = new JedisAircraftServer();//sinnlos
+		String channel = "ads.msg.velocity";
+		String message = "{\"subscribe\":[\"message\",\"ads.sentence\",\"1431889752.7486506!ADS-B*8D4C01E39901CF9BC00716FF7A55;\r\n\"]}";
+		//fixture.onMessage(channel, message);
+		Thread serverThread = new Thread(new Runnable(){
+		@Override
+		public void run() {
+			Jedis jedOne = new Jedis("localhost");
+			JedisAircraftServer myAircraftServer = new JedisAircraftServer();
+			jedOne.subscribe(myAircraftServer, "ads.msg.identification", "ads.msg.velocity", "ads.msg.position");
+		}});
+	
+	Jedis jedTwo = new Jedis("localhost");
+	jedTwo.flushAll();
+	serverThread.start();
+	jedTwo.publish(channel, ((AirborneVelocityMessage) AdsMessageFactory.getInstance().sentence2Message(message)).toJedisString());
+	Thread.sleep(2000);
+	
+	boolean result = jedTwo.exists("4981219");
+	assertEquals(result, true);
 	}
 
 	/**
